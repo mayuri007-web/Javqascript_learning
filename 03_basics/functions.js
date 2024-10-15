d4677cab0c3a610e00d16298f87c023c07d3710b
mayuri007-web <mayurinema@bitcot.com>
2024-10-15 09:33:11 +0000

    functions

---
+++ 03_basics/functions.js
@@ -0,0 +1,23 @@
+function sayMyName(){
+    console.log("M");
+    console.log("A");
+    console.log("Y");
+    console.log("U");
+    console.log("R");
+    console.log("I");
+}
+// sayMyName()
+
+// function AddTwoNumber(number1,number2){   //parameters
+//     console.log (number1+number2)
+// }
+// AddTwoNumber(6,"568")  //arguments
+
+function AddTwoNumber(number1,number2){   //parameters
+    const result =  number1 + number2
+    console.log("Mayuri")
+    return (result)
+    
+}
+const result = AddTwoNumber(6,4)
+console.log("Result ",result)
